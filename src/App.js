1c6c428bef42c7ed2eb64681ecc163ee7c8c5b02
portiaportia <portiaplante@gmail.com>
2026-06-08 14:54:39 -0400

    setup for gh-page deployment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
